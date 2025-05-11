ec7767216853bd9fe6978c9bd1e49ac173647888
Himanshu Sorathiya <sorathiyahim7827@gmail.com>
2025-05-11 21:44:35 +0530

    Installed react-toastify for toast notifications and feedback handling.

--- src/features/notification/Notification.tsx
+++ src/features/notification/Notification.tsx
@@ -3,14 +3,7 @@ import Header from "../../components/ui/Header.tsx";
 function Notification() {
   return (
     <>
-      <Header title="Notification">
-        <button
-          type="button"
-          className="hover:bg-dark-background cursor-pointer rounded bg-gray-800 px-8 py-3 font-semibold text-white transition-all duration-300"
-        >
-          Action
-        </button>
-      </Header>
+      <Header title="Notification">Action</Header>
     </>
   );
 }
