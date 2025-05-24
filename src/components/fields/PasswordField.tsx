6e5230884f6391ab08b80d4438ac58faaf06761b
Himanshu Sorathiya <sorathiyahim7827@gmail.com>
2025-05-24 10:28:09 +0530

    Implemented toggle visibility for password fields.

--- src/components/fields/PasswordField.tsx
+++ src/components/fields/PasswordField.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 import { useFieldContext } from "../../hooks/useAppForm.ts";
 
 import ErrorTooltip from "../ui/ErrorTooltip.tsx";
+import Icon from "../ui/Icon.tsx";
 
 function PasswordField({ label }: { label: string }) {
   const field = useFieldContext<string>();
+
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="relative flex flex-col gap-1">
@@ -11,18 +16,30 @@ function PasswordField({ label }: { label: string }) {
         {label}
       </label>
 
-      <input
-        id={field.name}
-        type="password"
-        value={field.state.value}
-        onBlur={field.handleBlur}
-        onChange={(e) => field.handleChange(e.target.value)}
-        className={`rounded-md px-4 py-3 caret-black outline-1 transition-all duration-100 focus:text-gray-700 focus:outline-gray-500 ${
-          field.state.value !== ""
-            ? "text-gray-700 outline-gray-500"
-            : "text-gray-500 outline-gray-400"
+      <div
+        className={`flex w-full cursor-text items-center gap-2 rounded-md px-4 py-3 text-left caret-black transition-all duration-100 ${
+          field.state.value
+            ? "text-gray-700 outline-1 outline-gray-500"
+            : "text-gray-500 outline-1 outline-gray-400"
         }`}
-      />
+      >
+        <input
+          id={field.name}
+          type={showPassword ? "text" : "password"}
+          value={field.state.value}
+          onBlur={field.handleBlur}
+          onChange={(e) => field.handleChange(e.target.value)}
+          className="h-full w-full bg-transparent outline-none"
+        />
+
+        <button type="button" onClick={() => setShowPassword(!showPassword)}>
+          {showPassword ? (
+            <Icon id="eye-on" className="size-5 cursor-pointer" />
+          ) : (
+            <Icon id="eye-off" className="size-5 cursor-pointer" />
+          )}{" "}
+        </button>
+      </div>
 
       {field.state.meta.isTouched && field.state.meta.errors && (
         <ErrorTooltip meta={field.state.meta} />
